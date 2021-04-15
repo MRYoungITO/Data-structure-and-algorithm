bc61a5f4aebe107af611903ad012dd2e0b274bd5
MRYoungITO <610139293@qq.com>
2021-04-15 23:23:25 +0800

    Exercise code on 15 April(Night)

--- boxman/boxman/boxman.cpp
+++ boxman/boxman/boxman.cpp
@@ -1,18 +1,9 @@
-#include <graphics.h>
 #include <iostream>
-#include <stdlib.h>
-#include <string>
-
-using namespace std;
-
-//墙:0
-//地板:1
-//箱子目的地:2
-//小人:3
-//箱子:4
-//箱子命中目标:5
+#include <graphics.h>
+#include <stdio.h>
 
 #define RATIO 61
+
 #define SCREEN_WIDTH 960
 #define SCREEN_HEIGHT 768
 
@@ -20,15 +11,29 @@ using namespace std;
 #define COLUMN 12
 
 #define START_X 100
-#define START_Y 150
+#define STRAT_Y 150
 
+enum _PROPS {
+	WALL,			//墙
+	FLOOR,		//地板
+	BOX_DES,	//箱子目的地
+	MAN,			//小人
+	BOX,			//箱子
+	HIT,				//箱子命中目标
+	ALL
+};
 
+using namespace std;
 
-IMAGE images[6];
+struct _POS {
+	int x;		//小人所在的二维数组的行
+	int y;		//小人所在的二维数组的列
+};
 
+IMAGE images[ALL];
 
+struct _POS man;
 
-/*游戏地图*/
 int map[LINE][COLUMN] = {
 	{0,0,0,0,0,0,0,0,0,0,0,0},
 	{0,1,0,1,1,1,1,1,1,1,0,0},
@@ -38,32 +43,32 @@ int map[LINE][COLUMN] = {
 	{0,1,1,1,0,3,1,1,1,4,1,0},
 	{0,1,2,1,1,4,1,1,1,1,1,0},
 	{0,1,0,0,1,0,1,1,0,0,1,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0}
 };
 
 int main(void) {
 	IMAGE bg_img;
-
-	//搭台唱戏
 	initgraph(SCREEN_WIDTH, SCREEN_HEIGHT);
-	loadimage(&bg_img, _T("blackground.bmp"), SCREEN_WIDTH, SCREEN_HEIGHT, true);
+	loadimage(&bg_img, _T("blackground.bmp"), 0, 0, true);
 	putimage(0, 0, &bg_img);
 
-	//加载道具图标
-	loadimage(&images[0], _T("wall_right.bmp"), RATIO, RATIO, true);
-	loadimage(&images[1], _T("floor.bmp"), RATIO, RATIO, true);
-	loadimage(&images[2], _T("des.bmp"), RATIO, RATIO, true);
-	loadimage(&images[3], _T("man.bmp"), RATIO, RATIO, true);
-	loadimage(&images[4], _T("box.bmp"), RATIO, RATIO, true);
-	loadimage(&images[5], _T("box.bmp"), RATIO, RATIO, true);
+	loadimage(&images[WALL], _T("wall_right.bmp"), RATIO, RATIO, true);
+	loadimage(&images[FLOOR], _T("floor.bmp"), RATIO, RATIO, true);
+	loadimage(&images[BOX_DES], _T("des.bmp"), RATIO, RATIO, true);
+	loadimage(&images[MAN], _T("man.bmp"), RATIO, RATIO, true);
+	loadimage(&images[BOX], _T("box.bmp"), RATIO, RATIO, true);
+	loadimage(&images[HIT], _T("bos.bmp"), RATIO, RATIO, true);
 
 	for (int i = 0; i < LINE; i++) {
 		for (int j = 0; j < COLUMN; j++) {
-			putimage(START_X + j * RATIO, START_Y + i * RATIO, &images[map[i][j]]);
+			if (map[i][j] == MAN) {
+				man.x = i;
+				man.y = j;
+			}
+			putimage(100 + j * 61, 150 + i * 61, &images[map[i][j]]);
 		}
-		cout << endl;
 	}
-	
+
 	system("pause");
 	return 0;
 }
