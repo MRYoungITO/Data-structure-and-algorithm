58243db222c42816df5ef4622d9c17a37163ee22
MRYoungITO <610139293@qq.com>
2021-05-31 23:43:19 +0800

    Exercise code on 31 May

---
+++ 类型运算符重载/类型运算符重载/Boy.cpp
@@ -0,0 +1,165 @@
+#include <sstream>
+#include "Boy.h"
+
+int Boy::LAST_ID = 0;
+
+Boy::Boy(const char* name, int age, int salary, int darkHorse)
+{
+	if (!name) {
+		name = "未命名";
+	}
+	this->name = new char[strlen(name) + 1];
+	strcpy_s(this->name, strlen(name) + 1, name);
+	this->age = age;
+	this->salary = salary;
+	this->darkHouse = darkHorse;
+	this->id = ++LAST_ID;
+}
+
+Boy::~Boy()
+{
+	if (name) {
+		delete name;
+	}
+}
+
+Boy::Boy(int salary)
+{
+	const char* defaultName = "未命名";
+
+	this->name = new char[strlen(defaultName) + 1];
+	strcpy_s(this->name, strlen(defaultName) + 1, defaultName);
+	this->age = 0;
+	this->salary = salary;
+	this->darkHouse = 0;
+	this->id = ++LAST_ID;
+}
+
+Boy::Boy(const char* name) 
+{
+	this->name = new char[strlen(name) + 1];
+	strcpy_s(this->name, strlen(name) + 1, name);
+
+	this->age = 0;
+	this->salary = 0;
+	this->darkHouse = 0;
+	this->id = ++LAST_ID;
+}
+
+string Boy::description()
+{
+	stringstream ret;
+	ret << "ID:" << id << "\t姓名:" << name << "\t年龄:" << age
+		<< "\t薪资:" << salary << "\t黑马系数:" << darkHouse;
+	return ret.str();
+}
+
+Boy& Boy::operator=(const Boy& boy)
+{
+	if (name) {
+		delete name;  //释放原来的内存
+	}
+	name = new char[strlen(boy.name) + 1];
+	strcpy_s(name, strlen(boy.name) + 1, boy.name);
+
+	this->age = boy.age;
+	this->salary = boy.salary;
+	this->darkHouse = boy.darkHouse;
+	//  this->id = boy.id;  // 根据需求来确定是否要拷贝id
+	return *this;
+}
+
+bool Boy::operator>(const Boy& boy)
+{
+	// 设置一个比较规则
+	// 薪资 * 黑马系数 + (100-年龄) * 1000
+	if (this->power() > boy.power()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool Boy::operator<(const Boy& boy)
+{
+	if (this->power() < boy.power()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool Boy::operator==(const Boy& boy)
+{
+	if (this->power() == boy.power()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+Boy::operator int() const
+{
+	return power();
+}
+
+Boy::operator char* () const 
+{
+	return this->name;
+}
+
+int Boy::operator[](string index)
+{
+	if (index == AGE_KEY) {
+		return age;
+	}
+	else if (index == SALARY_KEY) {
+		return salary;
+	}
+	else if (index == DARK_HORSE_KEY) {
+		return darkHouse;
+	}
+	else if (index == POWER_KEY) {
+		return power();
+	}
+	else {
+		return -1;
+	}
+}
+
+int Boy::operator[](int index)
+{
+	if (index == AGE) {
+		return age;
+	}
+	else if (index == SALARY) {
+		return salary;
+	}
+	else if (index == DARK_HORSE) {
+		return darkHouse;
+	}
+	else if (index == POWER) {
+		return power();
+	}
+	else {
+		return -1;
+	}
+}
+
+// 该方式不适合
+//ostream& Boy::operator<<(ostream& os) const
+//{
+//	os << "ID:" << id << "\t姓名:" << name << "\t年龄:" << age
+//		 << "\t薪资:" << salary << "\t黑马系数:" << darkHouse;
+//	return os;
+//}
+
+int Boy::power() const
+{
+	// 薪资 * 黑马系数 + (100-年龄) * 1000
+	int ret = salary * darkHouse + (100 - age) * 1000;
+	return ret;
+}
---
+++ 类型运算符重载/类型运算符重载/Boy.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+#define AGE_KEY "age"
+#define SALARY_KEY "salary"
+#define DARK_HORSE_KEY "darkHorse"
+#define POWER_KEY "power"
+
+// 枚举类型表示
+typedef enum {
+	AGE,
+	SALARY,
+	DARK_HORSE,
+	POWER
+}BOY_KEY_TYPE;
+
+using namespace std;
+
+class Boy
+{
+public:
+	Boy(const char* name, int age, int salary,
+		int darkHorse);
+	~Boy();
+
+	Boy(int salary);
+	Boy(const char* name);
+
+	string description();
+
+	// 重载赋值运算符
+	Boy& operator=(const Boy& boy);  //函数返回类型是 & 类型, 参数中加const保护实际参数不会被修改
+
+	bool operator>(const Boy& boy);
+	bool operator<(const Boy& boy);
+	bool operator==(const Boy& boy);
+
+	// 类型运算符重载, 不需要返回类型
+	operator int() const;
+	operator char* () const;
+
+	// 实现下标运算符的重载
+	int operator[](string index);
+	int operator[](int index);
+
+	//ostream& operator<<(ostream& os) const;
+	friend ostream& operator<<(ostream& os, const Boy& boy);
+	friend istream& operator>>(istream& is, Boy& boy);
+
+private:
+	char* name;
+	int age;
+	int salary;
+	int darkHouse;  // 黑马值, 表示潜力系数
+	unsigned int id;  //编号
+	static int LAST_ID;
+
+	int power() const;  // 综合能力值
+};
+
